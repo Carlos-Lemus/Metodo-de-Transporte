dc87921b2a502dc0bfd3398130e092eda1635932
carlos lemus <carlos-lemus-2000@hotmail.com>
2020-06-24 21:30:08 -0600

    Panel menu de la aplicacion

--- MetodoDeTransporte/MetodoDeTransporte/Form1.Designer.cs
+++ MetodoDeTransporte/MetodoDeTransporte/Form1.Designer.cs
@@ -28,13 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnSalir = new FontAwesome.Sharp.IconButton();
+            this.btnResultado = new FontAwesome.Sharp.IconButton();
+            this.btnInicio = new FontAwesome.Sharp.IconButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnHome = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.panelMenu.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnHome)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+            this.panelMenu.Controls.Add(this.btnSalir);
+            this.panelMenu.Controls.Add(this.btnResultado);
+            this.panelMenu.Controls.Add(this.btnInicio);
+            this.panelMenu.Controls.Add(this.panel1);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 521);
+            this.panelMenu.TabIndex = 1;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalir.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnSalir.FlatAppearance.BorderSize = 0;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSalir.IconChar = FontAwesome.Sharp.IconChar.SignInAlt;
+            this.btnSalir.IconColor = System.Drawing.Color.White;
+            this.btnSalir.IconSize = 32;
+            this.btnSalir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSalir.Location = new System.Drawing.Point(0, 461);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnSalir.Rotation = 0D;
+            this.btnSalir.Size = new System.Drawing.Size(220, 60);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnResultado
+            // 
+            this.btnResultado.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnResultado.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnResultado.FlatAppearance.BorderSize = 0;
+            this.btnResultado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResultado.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.btnResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResultado.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnResultado.IconChar = FontAwesome.Sharp.IconChar.PollH;
+            this.btnResultado.IconColor = System.Drawing.Color.White;
+            this.btnResultado.IconSize = 32;
+            this.btnResultado.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnResultado.Location = new System.Drawing.Point(0, 170);
+            this.btnResultado.Name = "btnResultado";
+            this.btnResultado.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnResultado.Rotation = 0D;
+            this.btnResultado.Size = new System.Drawing.Size(220, 60);
+            this.btnResultado.TabIndex = 2;
+            this.btnResultado.Text = "Resultado";
+            this.btnResultado.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnResultado.UseVisualStyleBackColor = true;
+            this.btnResultado.Click += new System.EventHandler(this.btnResultado_Click);
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnInicio.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnInicio.FlatAppearance.BorderSize = 0;
+            this.btnInicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInicio.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.btnInicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicio.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnInicio.IconChar = FontAwesome.Sharp.IconChar.Home;
+            this.btnInicio.IconColor = System.Drawing.Color.White;
+            this.btnInicio.IconSize = 32;
+            this.btnInicio.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInicio.Location = new System.Drawing.Point(0, 110);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnInicio.Rotation = 0D;
+            this.btnInicio.Size = new System.Drawing.Size(220, 60);
+            this.btnInicio.TabIndex = 1;
+            this.btnInicio.Text = "Inicio";
+            this.btnInicio.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnHome);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(220, 110);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnHome
+            // 
+            this.btnHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHome.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnHome.Image = global::MetodoDeTransporte.Properties.Resources.transporte;
+            this.btnHome.Location = new System.Drawing.Point(0, 0);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(220, 110);
+            this.btnHome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnHome.TabIndex = 0;
+            this.btnHome.TabStop = false;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.panel2.Controls.Add(this.labelTitulo);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(220, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(664, 70);
+            this.panel2.TabIndex = 2;
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(168)))), ((int)(((byte)(255)))));
+            this.labelTitulo.Location = new System.Drawing.Point(0, 0);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Padding = new System.Windows.Forms.Padding(19);
+            this.labelTitulo.Size = new System.Drawing.Size(291, 67);
+            this.labelTitulo.TabIndex = 0;
+            this.labelTitulo.Text = "Metodo de Transporte";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(884, 521);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "Form1";
+            this.Text = "Metodo de Transporte";
+            this.panelMenu.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnHome)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Panel panelMenu;
+        private FontAwesome.Sharp.IconButton btnResultado;
+        private FontAwesome.Sharp.IconButton btnInicio;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox btnHome;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label labelTitulo;
+        private FontAwesome.Sharp.IconButton btnSalir;
     }
 }
 
--- MetodoDeTransporte/MetodoDeTransporte/Form1.cs
+++ MetodoDeTransporte/MetodoDeTransporte/Form1.cs
@@ -7,14 +7,98 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FontAwesome.Sharp;
+
 
 namespace MetodoDeTransporte
 {
     public partial class Form1 : Form
     {
+        //bordes de los botones
+        private IconButton btnActual;
+        private Panel borderLeftBtn;
+        private Color colorBtn = Color.FromArgb(68, 189, 50);
+
+
         public Form1()
         {
             InitializeComponent();
+
+            btnSalir.ForeColor = Color.FromArgb(231, 76, 60);
+            btnSalir.IconColor = Color.FromArgb(231, 76, 60);
+
+            //agrego el border izquierdo al panel menu
+            borderLeftBtn = new Panel();
+            borderLeftBtn.Size = new Size(8, 90);
+            panelMenu.Controls.Add(borderLeftBtn);
+        }
+
+        //metodo que cambia las propiedades de los IconButton del panelMenu al activarse
+        private void activarBtn(object senderBtn, Color color)
+        {
+            //evaluo que el boton actual este activado
+            if (senderBtn != null)
+            {
+                desactivarBtn(); //desactivo el boton seleccionado anterios
+
+                //cambio las propiedades del boton y el borde izquierdo del boton
+
+                btnActual = (IconButton)senderBtn;
+                btnActual.BackColor = Color.FromArgb(44, 62, 80);
+                btnActual.ForeColor = color;
+                btnActual.TextAlign = ContentAlignment.MiddleCenter;
+                btnActual.IconColor = color;
+                btnActual.TextImageRelation = TextImageRelation.TextBeforeImage;
+                btnActual.ImageAlign = ContentAlignment.MiddleRight;
+                //border izquierdo del boton
+                borderLeftBtn.BackColor = color;
+                borderLeftBtn.Location = new Point(0, btnActual.Location.Y);
+                borderLeftBtn.Visible = true;
+                borderLeftBtn.BringToFront();
+            }
+
+        }
+
+        //metodo que cambia las propiedades de los IconButton del panelMenu al desactivarse
+        private void desactivarBtn()
+        {
+            //evaluo que el boton actual este activado
+            if (btnActual != null)
+            {
+
+                //cambio las propiedades del boton y el borde izquierdo del boton
+
+                btnActual.BackColor = Color.FromArgb(52, 73, 94);
+                btnActual.ForeColor = Color.White;
+                btnActual.TextAlign = ContentAlignment.MiddleLeft;
+                btnActual.IconColor = Color.White;
+                btnActual.TextImageRelation = TextImageRelation.ImageBeforeText;
+                btnActual.ImageAlign = ContentAlignment.MiddleLeft;
+                //border izquierdo del boton
+                borderLeftBtn.Visible = false;
+            }
+
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            activarBtn(sender, colorBtn); //activo el btn para cambiar sus propiedades
+        }
+
+        private void btnResultado_Click(object sender, EventArgs e)
+        {
+            activarBtn(sender, colorBtn); //activo el btn para cambiar sus propiedades
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            desactivarBtn(); //desactivo el boton seleccionado anterios
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close(); //cierro la ventana
         }
     }
 }
+
